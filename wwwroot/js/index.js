c0d386200545e008bbd4bbd6fcbd5a1d7c986915
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-12-29 21:57:34 +0000

    Add jquery and javascript

---
+++ wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    console.log("Hello from javascript");
+    var theForm = $("#the-form");
+    theForm.hide();
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying item");
+    });
+    var productInfo = $(".product-props li")
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(1000);
+    })
+});
