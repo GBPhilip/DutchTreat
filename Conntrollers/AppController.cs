d5d1f050bd23882837e0da55d4e5787f6b7472b2
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2021-12-31 09:28:45 +0000

    Add mvc index view of app controller

---
+++ Conntrollers/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new InvalidProgramException("Bad thinmgs happens to good developers");
+            return View();
+        }
+    }
+}
