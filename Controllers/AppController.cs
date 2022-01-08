e80dc93ce04c565e694501f6b69cab8af3df1024
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2022-01-08 18:35:41 +0000

    Validation added to contact form

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -22,6 +22,7 @@ namespace DutchTreat.Controllers
         [HttpPost("contact")]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (ModelState.IsValid) _mailService.SendMail("gbphilipsutton@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}")
             return View();
         }
         public IActionResult About()
