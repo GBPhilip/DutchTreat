8da8deef63fb7a61f9c705f9f24eef27c517f2a4
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2022-01-17 11:13:52 +0000

    Add database entities

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,12 @@
+using DutchTreat.Data;
 using DutchTreat.Services;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DutchTreat
 {
@@ -19,6 +16,10 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer();
+            });
             services.AddTransient<IMailService, NullMailService>();
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
