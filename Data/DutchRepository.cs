96574d7ad1ff23620bdbd82fc4f4dfd3acc46fd1
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2022-01-24 10:26:36 +0000

    logging

--- Data/DutchRepository.cs
+++ Data/DutchRepository.cs
@@ -1,5 +1,7 @@
 ﻿using DutchTreat.Data.Entities;
 
+using Microsoft.Extensions.Logging;
+
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,14 +11,17 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext context;
+        private readonly ILogger<DutchContext> logger;
 
-        public DutchRepository(DutchContext context)
+        public DutchRepository(DutchContext context, ILogger<DutchContext> logger)
         {
             this.context = context;
+            this.logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
+            logger.LogInformation("Get All Products was called");
             return context.Products.ToList(); ;
         }
 
