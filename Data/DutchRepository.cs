540c5df2cc43bb2ece77251beb7f932a15ca87fa
Philip Sutton <philip.sutton@routeco.com>
2022-01-25 16:40:01 +0000

    create api

--- Data/DutchRepository.cs
+++ Data/DutchRepository.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.Logging;
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -21,13 +22,30 @@ namespace DutchTreat.Data
 
         public IEnumerable<Product> GetAllProducts()
         {
-            logger.LogInformation("Get All Products was called");
-            return context.Products.ToList(); ;
+            try
+            {
+                logger.LogInformation("Get All Products was called");
+                return context.Products.ToList(); ;
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
         {
-            return context.Products.Where(p => p.Category == category).ToList(); ;
+            try
+            {
+                return context.Products.Where(p => p.Category == category).ToList();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
         public bool SaveAll()
