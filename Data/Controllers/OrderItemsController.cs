59726ac60c88468c2c2342843012b2e80aafc86d
Philip Sutton <philip.sutton@routeco.com>
2022-02-01 10:47:50 +0000

    Extend Order Api include order items and product

---
+++ Data/Controllers/OrderItemsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+
+using DutchTreat.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DutchTreat.Data.Controllers
+{
+    [Route("/api/orders/{orderid}/items")]
+    public class OrderItemsController : Controller
+    {
+        private readonly IDutchRepository repository;
+        private readonly ILogger<OrderItemsController> logger;
+        private readonly IMapper mapper;
+
+        public OrderItemsController(IDutchRepository repository,
+            ILogger<OrderItemsController> logger,
+            IMapper mapper)
+        {
+            this.repository = repository;
+            this.logger = logger;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int orderId)
+        {
+            var order = repository.GetOrderById(orderId);
+            if (order is not null) return Ok(mapper.Map<IEnumerable<OrderItemViewModel>>(order.Items));
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int orderId, int id)
+        {
+            var item = repository.GetOrderById(orderId)?.Items.FirstOrDefault(i => i.Id == id);
+            if (item is not null) return Ok(mapper.Map<OrderItemViewModel>(item));
+            return NotFound();
+        }
+    }
+}
