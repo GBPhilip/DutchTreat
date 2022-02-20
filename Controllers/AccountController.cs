e83a8806a4a1abf91ecb535a3f9ebe5098a5855d
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2022-02-20 18:06:13 +0000

    add login page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AccountController:Controller
+    {
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
