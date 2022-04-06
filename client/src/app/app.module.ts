e91327202be3d5cfb76532f93ce3963768903ce3
GBPhilip <10743454+GBPhilip@users.noreply.github.com>
2022-04-06 08:41:18 +0100

    products by angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { Store } from './services/store.service';
+import ProductListView from './views/productLisatView.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      ProductListView 
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule
   ],
-  providers: [],
+  providers: [Store],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
